e04856b44766d3ce29fede18d11a89c2d0214862
Giulio Garbi <giulio.garbi@gmail.com>
2018-03-26 16:19:13 +0200

    Updated cluster to the new inferastructure API

--- goat/clusterAgent.go
+++ goat/clusterAgent.go
@@ -2,6 +2,8 @@ package goat
 
 import(
     "net"
+    "time"
+    "sync"
 )
 
 type ClusterAgent struct{
@@ -9,23 +11,35 @@ type ClusterAgent struct{
     registrationAddress string
     componentId int
     firstMessageId int
-    chnGetMid chan bool
-    chnMids chan int
-    chnOutbox chan Message
-    chnInbox chan Message
+    chnGetMid *unboundChanUnit
+    chnMids *unboundChanInt
+    chnMessagesIn *unboundChanMessage
+    chnMessagesOut chan Message
     listeningPort int
     listener net.Listener
+    maxMid int
+    chnReceiveTime *unboundChanMT
+    chnSendTime *unboundChanMT
+    lockST *sync.Mutex
+    receiveTime map[int]int64
+    sendTime map[int]int64
 }
 
 func NewClusterAgent(messageQueueAddress string, registrationAddress string) *ClusterAgent{
     ca := ClusterAgent{
         messageQueueAddress: messageQueueAddress, 
         registrationAddress: registrationAddress,
-        chnGetMid: make(chan bool),
-        chnMids: make(chan int, 5),
-        chnOutbox: make(chan Message, 5),
-        chnInbox: make(chan Message, 5),
+        chnGetMid: newUnboundChanUnit(),
+        chnMids: newUnboundChanInt(),
+        chnMessagesIn: newUnboundChanMessage(),
+        chnMessagesOut: make(chan Message),
+        maxMid: -1,
         firstMessageId: -1,
+        chnReceiveTime: newUnboundChanMT(),
+        chnSendTime: newUnboundChanMT(),
+        lockST: &sync.Mutex{},
+        receiveTime: map[int]int64{},
+        sendTime: map[int]int64{},
     }
     return &ca
 }
@@ -40,14 +54,6 @@ func (ca *ClusterAgent) Start(){
     go ca.doIncomingProcess(chnRegistered)
     <- chnRegistered
     go ca.doOutcomingProcess()
-}
-
-func (ca *ClusterAgent) Inbox() <-chan Message{
-    return ca.chnInbox
-}
-
-func (ca *ClusterAgent) Outbox() chan<- Message{
-    return ca.chnOutbox
 }
 
 func (ca *ClusterAgent) GetComponentId() int{
@@ -68,7 +74,7 @@ func (ca *ClusterAgent) doIncomingProcess(chnRegistered chan<- struct{}) {
                 close(chnRegistered)
             case "RPLY":
                 mid := atoi(params[0])
-                ca.chnMids <- mid
+                ca.chnMids.In <- mid
                 
             case "DATA":
                 pred, _ := ToPredicate(params[2])
@@ -81,7 +87,14 @@ func (ca *ClusterAgent) doIncomingProcess(chnRegistered chan<- struct{}) {
                         Pred: pred,
                         Message: decodeTuple(params[3]),
                     }
-                    ca.chnInbox <- inMsg
+                    rtime := time.Now().UnixNano()
+                    ca.lockST.Lock()
+                    if mid > ca.maxMid{
+                        ca.maxMid = mid
+                    } 
+                    ca.lockST.Unlock()
+                    ca.chnReceiveTime.In <- msgTime{mid, rtime}
+                    ca.chnMessagesIn.In <- inMsg
                 }
         }
     }
@@ -92,22 +105,47 @@ func (ca *ClusterAgent) doOutcomingProcess() {
     //Work
     for {
         select {
-            case msgToSend := <- ca.chnOutbox:
+            case msgToSend := <- ca.chnMessagesOut:
+                stime := time.Now().UnixNano()
                 sendTo(ca.messageQueueAddress, "add", "DATA", itoa(msgToSend.Id), itoa(ca.componentId), msgToSend.Pred.String(), msgToSend.Message.encode() )
-            case <- ca.chnGetMid:
+                ca.lockST.Lock()
+                if msgToSend.Id >= ca.maxMid {
+                    ca.maxMid = msgToSend.Id
+                }
+                ca.lockST.Unlock()
+                ca.chnSendTime.In <- msgTime{msgToSend.Id, stime}
+            case <- ca.chnGetMid.Out:
                 sendTo(ca.messageQueueAddress, "add", "REQ", itoa(ca.componentId))
         }
     }
 }
 
-func (ca *ClusterAgent) GetMessageId() int{
-    ca.chnGetMid <- true
-    mid := <- ca.chnMids
-    if ca.firstMessageId < 0{
-        panic("Got a mid before getting the first message id!")
-    }
-    if mid < ca.firstMessageId {
-        panic("Got a mid < first message id!")
-    }
-    return mid
+func (ca *ClusterAgent) GetReceiveTime() map[int]int64{
+    ca.chnReceiveTime.Close()
+    return toMapIntInt64(&ca.receiveTime, ca.chnReceiveTime)
+}
+
+func (ca *ClusterAgent) GetSendTime() map[int]int64{
+    ca.chnSendTime.Close()
+    return toMapIntInt64(&ca.sendTime, ca.chnSendTime)
+}
+
+func (ca *ClusterAgent) GetMaxMid() int {
+    ca.lockST.Lock()
+    out := ca.maxMid
+    ca.lockST.Unlock()
+    return out 
+}
+
+func (ca *ClusterAgent) SendMessage(msg Message){
+    ca.chnMessagesOut <- msg
+}
+func (ca *ClusterAgent) AskMid(){
+    ca.chnGetMid.In <- struct{}{}
+}
+func (ca *ClusterAgent) GetRplyChan() *unboundChanInt {
+    return ca.chnMids
+}
+func (ca *ClusterAgent) GetDataChan() *unboundChanMessage {
+    return ca.chnMessagesIn
 }
