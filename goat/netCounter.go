da7433ee3509f1ef900117e2cdbcbb9d9999e4dd
Giulio Garbi <giulio.garbi@gmail.com>
2018-03-26 17:23:25 +0200

    Added perf test support to cluster

--- goat/netCounter.go
+++ goat/netCounter.go
@@ -2,16 +2,51 @@ package goat
 
 import (
     "net"
+    "sync/atomic"
 )
 
 type ClusterCounter struct {
     listener net.Listener
     count int
+    infrMessagesFromAgents uint64
+    infrMessagesSent uint64
+    perfTest bool
 }
 
-func NewClusterCounter(port int) *ClusterCounter{
+func NewClusterCounterPerf(perfTest bool, port int) *ClusterCounter{
     return &ClusterCounter{
         listener: listenToPort(port),
+        perfTest: perfTest,
+        infrMessagesFromAgents: 0,
+        infrMessagesSent: 0,
+    }
+}
+
+func (tn *ClusterCounter) onInfrMsgAgent() {
+    if tn.perfTest {
+        atomic.AddUint64(&tn.infrMessagesFromAgents, 1)
+    }
+}
+
+func (tn *ClusterCounter) onInfrMsgSent() {
+    if tn.perfTest {
+        atomic.AddUint64(&tn.infrMessagesSent, 1)
+    }
+}
+
+func (tn *ClusterCounter) GetInfrMsgAgent() uint64 {
+    if tn.perfTest {
+        return atomic.LoadUint64(&tn.infrMessagesFromAgents)
+    } else {
+        panic("No performance tests required!")
+    }
+}
+
+func (tn *ClusterCounter) GetInfrMsgSent() uint64 {
+    if tn.perfTest {
+        return atomic.LoadUint64(&tn.infrMessagesSent)
+    } else {
+        panic("No performance tests required!")
     }
 }
 
@@ -27,10 +62,12 @@ func (cc *ClusterCounter) Work(timeout int64, timedOut chan<- struct{}){
             case "read": // ask, it will not be used
                 rplPort := params[0]
                 rplAddress := netAddress{srcAddr.Host, rplPort}
+                cc.onInfrMsgSent()
                 sendToAddress(rplAddress, "count", itoa(cc.count))
             case "inc": // it will be assigned to a message
                 rplPort := params[0]
                 rplAddress := netAddress{srcAddr.Host, rplPort}
+                cc.onInfrMsgSent()
                 sendToAddress(rplAddress, "count", itoa(cc.count))
                 cc.count++
         }    
